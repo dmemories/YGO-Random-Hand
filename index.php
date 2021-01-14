8cb8bc120b8ba6a6018cef4067738c47b4837698
dmemories <spyhack13@yahoo.com>
2021-01-14 21:26:59 +0700

    remove integrity

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
 <head>
   <title>YGO Random Hand</title>
   <meta charset="utf-8">
-  <link href="<?=PATH_CSS;?>bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+  <link href="<?=PATH_CSS;?>bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
 
@@ -104,7 +104,7 @@
 
 ?>
 
-<script src="<?=PATH_SCRIPT;?>popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
-<script src="<?=PATH_SCRIPT;?>https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
+<script src="<?=PATH_SCRIPT;?>popper.min.js"></script>
+<script src="<?=PATH_SCRIPT;?>bootstrap.min.js"></script>
 </body>
 </html>
